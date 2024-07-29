500b8e8217cd861e9a3e29ae7e99e0290aba9e40
fortune <basket85r@gmail.com>
2024-07-29 15:47:04 +0900

    メッセージエリアのコードをリファクタリング

--- ChatAppPrctice/Views/MessageRow.swift
+++ ChatAppPrctice/Views/MessageRow.swift
@@ -10,23 +10,43 @@ import SwiftUI
 struct MessageRow: View {
     var body: some View {
         HStack(alignment: .top) {
-            Image(systemName: "person.circle")
-                .resizable()
-                .frame(width: 48, height: 48)
-            Text("こんにちは")
-                .padding()
-                .background(.white)
-                .cornerRadius(30)
-            VStack(alignment: .trailing) {
-                Spacer()
-                Text("既読")
-                Text(formattedDataString)
-            }
-            .foregroundColor(.secondary)
-            .font(.footnote)
+            userThumb
+            messageText
+            messageState
             Spacer()
         }
         .padding(.bottom)
+    }
+}
+
+#Preview {
+    MessageRow()
+        .background(.cyan)
+}
+
+extension MessageRow {
+    
+    private var userThumb: some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .frame(width: 48, height: 48)
+    }
+    
+    private var messageText: some View {
+        Text("こんにちは")
+            .padding()
+            .background(.white)
+            .cornerRadius(30)
+    }
+    
+    private var messageState: some View {
+        VStack(alignment: .trailing) {
+            Spacer()
+            Text("既読")
+            Text(formattedDataString)
+        }
+        .foregroundColor(.secondary)
+        .font(.footnote)
     }
     
     private var formattedDataString: String {
@@ -34,9 +54,4 @@ struct MessageRow: View {
         formatter.timeStyle = .short
         return formatter.string(from: Date())
     }
-}
-
-#Preview {
-    MessageRow()
-        .background(.cyan)
 }
