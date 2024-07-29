0b6de10d17bf05e2d790a83b9dab7d2e0da02f7a
fortune <basket85r@gmail.com>
2024-07-29 17:37:45 +0900

    JSONデータを変換する処理を追加

--- ChatAppPrctice/Models/Chat.swift
+++ ChatAppPrctice/Models/Chat.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Caht {
+struct Chat: Decodable {
     let id: String
     let messages: [Message]
 }
--- ChatAppPrctice/ViewModels/ChatViewModel.swift
+++ ChatAppPrctice/ViewModels/ChatViewModel.swift
@@ -9,4 +9,25 @@ import Foundation
 
 class ChatViewModel {
     
+    private func fetchChatData() -> [Chat] {
+        let fileName = "chatData.json"
+        let data: Data
+        
+        guard let filePath = Bundle.main.url(forResource: fileName, withExtension: nil)
+        else {
+            fatalError("\(fileName)が見つかりません")
+        }
+        
+        do {
+            data = try Data(contentsOf: filePath)
+        } catch {
+            fatalError("\(fileName)のロードに失敗しました")
+        }
+        
+        do {
+            return try JSONDecoder().decode([Chat].self, from: data)
+        } catch {
+            fatalError("\(fileName)を\(Chat.self)に変換することに失敗しました")
+        }
+    }
 }
