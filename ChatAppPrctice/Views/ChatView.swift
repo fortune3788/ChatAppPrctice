7bc51495dd68f579687806dfbcdc4b2faf4d8180
fortune <basket85r@gmail.com>
2024-07-30 00:59:24 +0900

    メッセージ投稿機能の改善

--- ChatAppPrctice/Views/ChatView.swift
+++ ChatAppPrctice/Views/ChatView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ChatView: View {
     
     @State private var textFieldText: String = ""
+    @FocusState private var textFieldFocused: Bool
     
     @ObservedObject var vm: ChatViewModel = ChatViewModel()
     
@@ -43,6 +44,9 @@ extension ChatView {
             .padding(.top, 72)
         }
         .background(Color("Background"))
+        .onTapGesture {
+            textFieldFocused = false
+        }
     }
     
     private var inputArea: some View {
@@ -68,6 +72,7 @@ extension ChatView {
                 .onSubmit {
                     sendMessage()
                 }
+                .focused($textFieldFocused)
            Image(systemName: "mic")
                 .font(.title2)
         }
@@ -95,6 +100,9 @@ extension ChatView {
     }
     
     private func sendMessage() {
-        vm.addMessage(text: textFieldText)
+        if !textFieldText.isEmpty {
+            vm.addMessage(text: textFieldText)
+            textFieldText = ""
+        }
     }
 }
