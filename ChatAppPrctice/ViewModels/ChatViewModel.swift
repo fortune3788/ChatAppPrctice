9994b7fe9ddfcba784d1a196ed722994f91be134
fortune <basket85r@gmail.com>
2024-07-30 00:46:10 +0900

    新規メッセージを追加できるようにした

--- ChatAppPrctice/ViewModels/ChatViewModel.swift
+++ ChatAppPrctice/ViewModels/ChatViewModel.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-class ChatViewModel {
+class ChatViewModel: ObservableObject {
     
     var chatData: [Chat] = []
-    var messages: [Message] = []
+    @Published var messages: [Message] = []
     
     init() {
         chatData = fetchChatData()
@@ -45,5 +45,7 @@ class ChatViewModel {
                                  user: User.currentUser,
                                  date: Date().description,
                                  readed: false)
+        
+        messages.append(newMessage)
     }
 }
