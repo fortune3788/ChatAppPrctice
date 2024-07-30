74d8bd4a9c6f15d37506f46d8db711b88c11deee
fortune <basket85r@gmail.com>
2024-07-30 23:57:58 +0900

    リスト画面用に新しいファイルを作成

---
+++ ChatAppPrctice/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  ChatAppPrctice
+//
+//  Created by 福原塁 on 2024/07/30.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
