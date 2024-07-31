a8c7c5921b0f4aa7274cf8f5fa495df0e943adc3
fortune <basket85r@gmail.com>
2024-07-31 18:48:39 +0900

    メッセージ投稿後に画面をスクロールさせる

--- ChatAppPrctice/Views/ChatView.swift
+++ ChatAppPrctice/Views/ChatView.swift
@@ -56,6 +56,9 @@ extension ChatView {
             .onAppear{
                 scrollToLast(proxy: proxy)
             }
+            .onChange(of: chat.messages) { 
+                scrollToLast(proxy: proxy, smooth: true)
+            }
         }
     }
     
@@ -121,10 +124,15 @@ extension ChatView {
         }
     }
     
-    private func scrollToLast(proxy: ScrollViewProxy) {
+    private func scrollToLast(proxy: ScrollViewProxy, smooth: Bool = false) {
         if let lastMessage = chat.messages.last {
-            proxy.scrollTo(lastMessage.id, anchor: .bottom)
+            if smooth {
+                withAnimation(.smooth) {
+                    proxy.scrollTo(lastMessage.id, anchor: .bottom)
+                }
+            } else {
+                proxy.scrollTo(lastMessage.id, anchor: .bottom)
+            }
         }
-        
     }
 }
